636ce9e5e58baeea57952cb9ba3320d0c954d28f
Hugo Radulovich <hcradulovich@gmail.com>
2022-11-14 19:08:23 -0300

    revisando errores

--- src/AdviceApp.jsx
+++ src/AdviceApp.jsx
@@ -1,67 +1,52 @@
-
-
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 
 export const AdviceApp = () => {
 
-    const [advice, setAdvice] = useState('');
-    const [id, setId] = useState('');
-  
-    // console.log(advice);
-    
-    
-    async function getUser() {
-        try {
-          const response = await axios.get('https://api.adviceslip.com/advice');
-          setAdvice(response.data.slip.advice)
-          setId(response.data.slip.id)
-          // const {advice,id} = response.data.slip;
-          console.log(advice)
-          return (
-            id
-          )
-          // console.log(response);
-        } catch (error) {
-          console.error(error);
-        }
-      }
+const [id, setId] = useState();
+const [advice, setAdvice] = useState('');
 
-      useEffect(() => {
-      
-        getUser();
-        
-      }, [setId])
-      
-  
-      
-    return (
-        <>
-            
-              <div className="container">
-                <div className="position-absolute top-50 start-50 translate-middle">
-                <div className="card card-w rounded-4">
-                  <div className="card-body">
-                    <h5 className="card-title text-center tex-id-color">Advice #{id}</h5>
-                    <p className="card-text fs-2 fw-bold text-center tex-adv-color">{advice}</p>
-                    <div className="d-flex justify-content-center aling-item-center">
-                    <img src="src\images\pattern-divider-desktop.svg" className='m-4 ' alt="" />
-                    </div>
-                    <button onClick={getUser} className=' position-absolute top-100 start-50 translate-middle btn-color rounded-5'>
-                        <img src="src\images\icon-dice.svg" alt="boton_advice" />
-                    </button>
-                    
-                    
-                  </div>
-                </div>
-                </div>
-                
-              </div>
 
+const getAdvice = async() => {
+  const url = `https://api.adviceslip.com/advice`;
+  const resp =  await fetch(url);
+  const data = await resp.json();
+  setId(data.slip.id);
+  setAdvice(data.slip.advice)
+}
+
+useEffect(() => {
+  getAdvice();
+}, [])
+
+console.log(id);
+console.log(advice)
+  return (  
+    <>
             
-            
-            
-        </>
-    )
+    <div className="container">
+      <div className="position-absolute top-50 start-50 translate-middle">
+      <div className="card card-w rounded-4">
+        <div className="card-body">
+          <h5 className="card-title text-center tex-id-color">Advice #{id}</h5>
+          <p className="card-text fs-2 fw-bold text-center tex-adv-color">{advice}</p>
+          <div className="d-flex justify-content-center aling-item-center">
+          <img src="src\images\pattern-divider-desktop.svg" className='m-4 ' alt="" />
+          </div>
+          <button onClick={getAdvice} className=' position-absolute top-100 start-50 translate-middle btn-color rounded-5'>
+              <img src="src\images\icon-dice.svg" alt="boton_advice" />
+          </button>
+          
+          
+        </div>
+      </div>
+      </div>
+      
+    </div>
+
+  
+  
+  
+</>
+  )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AdviceApp } from './AdviceApp'
-
 import './index.css'
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <AdviceApp />
-  </React.StrictMode>
+  // </React.StrictMode>
 )
